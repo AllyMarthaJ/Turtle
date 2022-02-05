64b2641a18f80459485df2ca1fbd99ed331c4aac
AllyMarthaJ <alex@allym.me>
2022-02-05 22:01:00 +1100

    Fixed up formatting, begin work on formattedstring

--- Turtle/Graphics/FormattedString.cs
+++ Turtle/Graphics/FormattedString.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Turtle.Graphics
 {
 	public class FormattedString
 	{
+		public const char ANSI_PREFIX = '\x1b';
+		public readonly StringBuilder RESET_FORMAT = new Format ().GetFormatBuilder();
+
 		public string RawValue { get; set; }
 
-		public Dictionary<Regex, Format> Formats { get; set; }
+		public Dictionary<Regex, Format> Formats { get; set; } = new ();
 
 		public FormattedString (string value)
 		{
@@ -17,7 +21,13 @@ namespace Turtle.Graphics
 		public static implicit operator string(FormattedString s)
 		{
 			// TODO
-			return null;
+			StringBuilder sb = new ();
+
+			foreach (var line in s.RawValue.Split (Environment.NewLine)) {
+				sb.AppendLine (s.formatLine (line));
+			}
+
+			return sb.ToString ();
 		}
 
 		public static implicit operator FormattedString(string s)
@@ -27,6 +37,20 @@ namespace Turtle.Graphics
 
 		private string formatLine(string line)
 		{
+			foreach (var format in this.Formats) {
+				format.Key.Replace(line, m => {
+					var fmtBuilder = format.Value.GetFormatBuilder ();
+
+					fmtBuilder.Insert (0, ANSI_PREFIX);
+					fmtBuilder.Append (m.Value);
+					fmtBuilder.Append (ANSI_PREFIX);
+					fmtBuilder.Append (RESET_FORMAT);
+
+					return fmtBuilder.ToString ();
+				});
+			}
+
+			return line;
 		}
 	}
 }
