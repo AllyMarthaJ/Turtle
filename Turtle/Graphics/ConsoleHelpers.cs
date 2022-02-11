bac47fddc20d02e9ae97c66977a8b2126418fec3
AllyMarthaJ <alex@allym.me>
2022-02-11 14:39:24 +1100

    Grid drawing works now! Added alternate screen mode

--- Turtle/Graphics/ConsoleHelpers.cs
+++ Turtle/Graphics/ConsoleHelpers.cs
@@ -1,7 +1,25 @@
 ﻿using System;
+using System.Text;
+
 namespace Turtle.Graphics {
 	public class ConsoleHelpers {
 		public const string ANSI_PREFIX = "\x1b[";
+
+		private static bool alternate = false;
+		public static bool AlternateScreen {
+			get {
+				return alternate;
+			}
+			set {
+				StringBuilder sb = new ();
+				sb.Append (ANSI_PREFIX);
+				sb.Append ("?1049");
+				sb.Append (value ? "h" : "l");
+				Console.Write (sb.ToString ());
+
+				alternate = value;
+			}
+		}
 
 		public static void WriteInPlace (FormattedString input, Offset offset)
 		{
@@ -12,21 +30,21 @@ namespace Turtle.Graphics {
 			var fmLines = formattedInput.Split (Environment.NewLine);
 
 			for (int i = 0; i < fmLines.Length; i++) {
-				var offsetLine = InPlaceOffset (fmLines [i], offset);
+				var offsetLine = GetPositionedString (fmLines [i], offset);
 
 				Console.Write (offsetLine);
 
 				offset = new Offset (-rawLines [i].Length, 1);
 			}
 
-			var ret = InPlaceOffset ("", new Offset (
+			var ret = GetPositionedString ("", new Offset (
 					-rawLines [rawLines.Length - 1].Length - initOffset.X,
 					-rawLines.Length + 1 - initOffset.Y))
 				;
 			Console.Write (ret);
 		}
 
-		public static string InPlaceOffset (string str, Offset offset)
+		public static string GetPositionedString (string str, Offset offset)
 		{
 			foreach (var offsetBuilder in offset.GetOffsetBuilders ()) {
 				offsetBuilder.Insert (0, ConsoleHelpers.ANSI_PREFIX);
