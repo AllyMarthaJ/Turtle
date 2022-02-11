719b3b67aa1c81dda567446a4a542e77715578af
AllyMarthaJ <ally@piecewise.org>
2022-02-12 00:08:05 +1100

    Makes sure day switching works

--- Turtle/TurtleGame.cs
+++ Turtle/TurtleGame.cs
@@ -17,7 +17,7 @@ namespace Turtle {
 		public CharHint [] [] GameState { get; }
 
 		public TurtleGame (IGenerator generator, bool hard = false) :
-			this (generator, (DateTime.Today.ToUniversalTime() - DateTime.UnixEpoch).Milliseconds, hard)
+			this (generator, (int)(DateTime.Today.ToUniversalTime() - DateTime.UnixEpoch).TotalSeconds, hard)
 		{ }
 
 		public TurtleGame (IGenerator generator, int seed, bool hard = false)
