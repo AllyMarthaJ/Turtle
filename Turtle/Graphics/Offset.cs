a6dbfbd18f1cd67fb3b1a99ea0bfd0b177ab146e
AllyMarthaJ <alex@allym.me>
2022-02-11 13:55:51 +1100

    Finally got positioning with ANSI right

--- Turtle/Graphics/Offset.cs
+++ Turtle/Graphics/Offset.cs
@@ -1,33 +1,36 @@
 ﻿using System;
 using System.Text;
 
-namespace Turtle.Graphics
-{
-	public struct Offset
-	{
+namespace Turtle.Graphics {
+	public struct Offset {
 		public int? X { get; set; }
 		public int? Y { get; set; }
 
-		public Offset(int? x = null, int? y = null)
+		public Offset (int? x = null, int? y = null)
 		{
 			this.X = x;
 			this.Y = y;
 		}
 
-		public StringBuilder[] GetOffsetBuilders ()
+		public StringBuilder [] GetOffsetBuilders ()
 		{
-			StringBuilder [] builders = new StringBuilder[X.HasValue && Y.HasValue ? 2 : 1];
+			bool hasX = X.HasValue && X.Value != 0;
+			bool hasY = Y.HasValue && Y.Value != 0;
+
+			StringBuilder [] builders = new StringBuilder [
+				hasX && hasY ? 2 :
+				hasX || hasY ? 1 : 0];
 
 			int pos = 0;
-			if (X.HasValue) {
+			if (hasX) {
 				builders [pos] = new StringBuilder ();
-				builders [pos].Append ($"{Math.Abs(X.Value)};");
+				builders [pos].Append ($"{Math.Abs (X.Value)};");
 				builders [pos].Append (X.Value >= 0 ? 'C' : 'D');
 
 				pos++;
 			}
 
-			if (Y.HasValue) {
+			if (hasY) {
 				builders [pos] = new StringBuilder ();
 				builders [pos].Append ($"{Math.Abs (Y.Value)};");
 				builders [pos].Append (Y.Value >= 0 ? 'B' : 'A');
