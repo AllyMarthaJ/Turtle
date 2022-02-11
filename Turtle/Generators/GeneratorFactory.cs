ddbae4f76ebfe213003239dbaf0d800c160858e1
AllyMarthaJ <ally@piecewise.org>
2022-02-11 22:06:44 +1100

    Continue adding support

--- Turtle/Generators/GeneratorFactory.cs
+++ Turtle/Generators/GeneratorFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 using Turtle.Env;
 
 namespace Turtle.Generators
@@ -16,34 +18,44 @@ namespace Turtle.Generators
 		{
 			string url = urlOrUuid;
 
-			if (Guid.TryParse(urlOrUuid, out Guid guid)) {
+			// if we fail to create a uri, fetch the repo and then grab the relevant url
+			if (!Uri.TryCreate(urlOrUuid, UriKind.Absolute, out _)) {
 				var repo = await this.fetchRepository ();
 
-				url = repo [guid];
+				url = repo [url];
 			}
 
+			// make a request to either the repo url, or direct url
 			var sourceResp = await fetchClient.GetAsync (url);
 			return await sourceResp.Content.ReadAsStringAsync ();
 		}
 
-		private async Task<Dictionary<Guid, string>> fetchRepository()
+		private async Task<Dictionary<string, string>> fetchRepository()
 		{
+			// fetch the repo
 			var response = await fetchClient.GetAsync (VARS.REPO);
 
+			// blegh
 			if (!response.IsSuccessStatusCode) {
-				return new Dictionary<Guid, string> ();
+				return new Dictionary<string, string> ();
 			}
 
+			// convert everything to a nice neat object oriented form
 			var content = await response.Content.ReadAsStringAsync();
 
-			var repository = new Dictionary<Guid, string> ();
+			var repository = new Dictionary<string, string> ();
 
 			foreach (var line in content.Split(Environment.NewLine)) {
 				var entry = line.Split (" : ");
-				repository.Add (Guid.Parse (entry[0]), entry [1]);
+				repository.Add (entry[0], entry [1]);
 			}
 
 			return repository;
+		}
+
+		public IGenerator CompileGeneratorSource(string source)
+		{
+			CSharpScript.Create (source, ScriptOptions.Default.WithReferences (typeof (IGenerator).Assembly));
 		}
 	}
 }
